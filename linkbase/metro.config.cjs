3221a53f4a596d1b9b0703db4fa5bdc04859aeec
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2025-07-19 22:00:19 +0300

    WIP fixing metro build

--- linkbase/metro.config.cjs
+++ linkbase/metro.config.cjs
@@ -4,8 +4,19 @@ const path = require("path");
 
 const projectRoot = __dirname;
 const workspaceRoot = path.resolve(projectRoot, "..");
+const linkbaseRoot = path.resolve(projectRoot, "linkbase");
+const packagesRoot = path.resolve(workspaceRoot, "packages");
+const sharedRoot = path.resolve(packagesRoot, "shared");
+const prismaRoot = path.resolve(packagesRoot, "prisma");
 
 const config = getDefaultConfig(__dirname);
-config.watchFolders = [path.resolve(workspaceRoot, "packages")];
+config.watchFolders = [packagesRoot];
+
+config.resolver.nodeModulesPaths = [
+  path.resolve(linkbaseRoot, "node_modules"),
+  path.resolve(workspaceRoot, "node_modules"),
+  path.resolve(sharedRoot, "node_modules"),
+  path.resolve(prismaRoot, "node_modules"),
+];
 
 module.exports = config;
