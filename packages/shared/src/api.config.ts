e078537c41bb4fc3978721b86e6b47cba4bd2ca2
Kaloyan D. Bozhkov <kaloyan@bozhkov.com>
2025-07-25 13:29:34 +0300

    .

--- packages/shared/src/api.config.ts
+++ packages/shared/src/api.config.ts
@@ -5,7 +5,7 @@ export const getApiBaseUrl = (__DEV__: boolean) => {
     // In development, use the detected IP for physical devices and simulators
     return `http://${DEV_HOST_IP}:3000/api`;
   } else {
-    // In production, you'd use your actual production API URL
-    return `https://linkbase-kappa.vercel.app/api`; // @TODO vercel url env
+    // In production point to VPS
+    return `https://linkbase.kaloyanbozhkov.com/api`;
   }
 };
